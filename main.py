13a7a8c65187a0ebdaa3f6bab49219e5c22a6519
bristolkon <bristolkon3802@gmail.com>
2023-09-01 22:27:36 +0900

    Flask templates 생성

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from extractors.wwr import extract_wwr_jobs
 from extractors.indeed import extract_indeed_jobs
 from file import save_to_file
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
 @app.route("/")
 def home():
-    return 'hey there!'
+    return render_template("home.html",name="Bing")
 
-app.run("0.0.0.0")
+@app.route("/hello")
+def hello():
+    return 'hello you!'
+
+app.run("127.0.0.1")
 
 """
 keyword = input("직업을 입력 해주세요?")
